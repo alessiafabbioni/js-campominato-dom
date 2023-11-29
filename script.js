ffa407320d8f31a5f527624904d20901b6238e58
alessiafabbioni <alessiafabbioni@gmail.com>
2023-11-29 17:08:40 +0100

    grid & bomb definition

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
+// Il computer deve generare 16 numeri casuali nello stesso 
+// range della difficoltà prescelta: le bombe. 
+
+
+
+/*In seguito l’utente clicca su una cella: se il numero è presente 
+nella lista dei numeri generati - abbiamo calpestato una bomba - 
+la cella si colora di rosso e la partita termina.
+Altrimenti la cella cliccata si colora di azzurro e 
+l’utente può continuare a cliccare sulle altre celle.*/
+
+
+
+/*La partita termina quando il giocatore clicca su una 
+bomba o quando raggiunge il numero massimo possibile di 
+numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il 
+punteggio, cioè il numero di volte che l’utente ha cliccato 
+su una cella che non era una bomba.*/
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     const gridElement = document.getElementById("gridContainer");
-
-
     const playButton = document.querySelector('button');
 
     
     playButton.addEventListener('click', function () {
-       
+    
+        //const arryRandomNum = genArrayRandomNum (1, 100, 100);
+
+        gridElement.innerHTML = "";
+
+        const gridLength = 100;
+        const nBombs = 16;
+        
+
+
+
         for (let i = 1; i <= 100; i++) {
             const newElement = createmyElement("div", "grid-box");
             newElement.textContent = i;
@@ -30,15 +59,27 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-// Il computer deve generare 16 numeri casuali nello stesso 
-// range della difficoltà prescelta: le bombe. 
+
+//Funzione ordinamento randomico array
+
+/*function genArrayRandomNum (minNum, maxNum, lengthArr) {
+    const arryRandomNum = [];
+
+    while (arryRandomNum.length < lengthArr) {
+        let genNum = randomNum(minNum, maxNum);
+        
+        if (!arryRandomNum.includes(newNumber)) {
+            arryRandomNum.push(newNumber);
+        }
+    }
+
+    return arryRandomNum;
+}
+*/
 
 
+//Funzione creazione numeri random
 
-/*In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
-Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.*/
-
-
-
-/*La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.*/
+function randomNum (min, max) {
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
